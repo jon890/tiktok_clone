4a475b85a21d9650b33d55ae7571209af0fbe8f2
BiFoS <jon89071@gmail.com>
2023-06-26 16:43:56 +0900

    :truck: Email Screen => Username Screen

--- lib/features/authentication/username_screen.dart
+++ lib/features/authentication/username_screen.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 
-class EmailScreen extends StatelessWidget {
-  const EmailScreen({super.key});
+import '../../constants/sizes.dart';
+
+class UsernameScreen extends StatelessWidget {
+  const UsernameScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: const Text("Sign up")
-        )
+      appBar: AppBar(
+        title: const Text("Sign up"),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: Sizes.size36),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            Text("Create username",
+                style: TextStyle(
+                    fontSize: Sizes.size24, fontWeight: FontWeight.w700)),
+            Gaps.v8,
+            Text("You can always change this later.",
+                style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: Sizes.size16,
+                    fontWeight: FontWeight.w600))
+          ],
+        ),
+      ),
     );
   }
 }
