71de2709651749c4d56528acf80bcafdae453beb
BiFoS <jon89071@gmail.com>
2023-06-27 10:58:29 +0900

    :lipstick: Create Username screen

--- lib/features/authentication/username_screen.dart
+++ lib/features/authentication/username_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
 
 import '../../constants/sizes.dart';
 
-class UsernameScreen extends StatelessWidget {
+class UsernameScreen extends StatefulWidget {
   const UsernameScreen({super.key});
+
+  @override
+  State<UsernameScreen> createState() => _UsernameScreenState();
+}
+
+class _UsernameScreenState extends State<UsernameScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+
+  String _username = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    _usernameController.addListener(() {
+      setState(() {
+        _username = _usernameController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+
+    // 마지막에 super.dispose()를 해라
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,23 +41,33 @@ class UsernameScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Sign up"),
       ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: Sizes.size36),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Gaps.v40,
-            Text("Create username",
-                style: TextStyle(
-                    fontSize: Sizes.size24, fontWeight: FontWeight.w700)),
-            Gaps.v8,
-            Text("You can always change this later.",
-                style: TextStyle(
-                    color: Colors.black54,
-                    fontSize: Sizes.size16,
-                    fontWeight: FontWeight.w600))
-          ],
-        ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Gaps.v40,
+          const Text("Create username",
+              style: TextStyle(
+                  fontSize: Sizes.size24, fontWeight: FontWeight.w700)),
+          Gaps.v8,
+          const Text("You can always change this later.",
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: Sizes.size16,
+                  fontWeight: FontWeight.w600)),
+          Gaps.v16,
+          TextField(
+            controller: _usernameController,
+            cursorColor: Theme.of(context).primaryColor,
+            decoration: InputDecoration(
+                hintText: "Username",
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade400)),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade400))),
+          ),
+          Gaps.v28,
+          FormButton(disabled: _username.isEmpty)
+        ]),
       ),
     );
   }
