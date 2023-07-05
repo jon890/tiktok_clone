0101563b910e6c2f965b708fef488d44b83e21e9
BiFoS <jon89071@gmail.com>
2023-07-05 11:41:34 +0900

    :lipstick: Play Button with animation!
    with AnimationController

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -35,7 +35,7 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
       scrollDirection: Axis.vertical,
       onPageChanged: _onPageChanged,
       itemBuilder: (context, index) {
-        return VideoPost(onVideoFinished: _onVideoFinished);
+        return VideoPost(onVideoFinished: _onVideoFinished, index: index);
       },
     );
   }
