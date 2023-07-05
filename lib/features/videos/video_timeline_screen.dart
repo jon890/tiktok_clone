04badf0133d28aff7563c3ef672d19f38fe6c387
BiFoS <jon89071@gmail.com>
2023-07-05 10:32:53 +0900

    :lipstick: VideoPlayer

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/features/videos/widgets/video_post.dart';
 
 class VideoTimelineScreen extends StatefulWidget {
   const VideoTimelineScreen({super.key});
@@ -8,42 +9,33 @@ class VideoTimelineScreen extends StatefulWidget {
 }
 
 class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
-  int _itemCount = 4;
-
-  List<Color> colors = [
-    Colors.blue,
-    Colors.red,
-    Colors.yellow,
-    Colors.teal,
-  ];
+  final _pageController = PageController();
+  final _scrollDuration = const Duration(milliseconds: 200);
+  final _scrollCurve = Curves.linear;
 
   void _onPageChanged(int page) {
-    if (page == _itemCount - 1) {
-      _itemCount += 4;
-      colors.addAll([
-        Colors.blue,
-        Colors.red,
-        Colors.yellow,
-        Colors.teal,
-      ]);
-      setState(() {});
-    }
+    _pageController.animateToPage(page,
+        duration: _scrollDuration, curve: _scrollCurve);
+  }
+
+  void _onVideoFinished() {
+    _pageController.nextPage(duration: _scrollDuration, curve: _scrollCurve);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
+      controller: _pageController,
       scrollDirection: Axis.vertical,
       onPageChanged: _onPageChanged,
       itemBuilder: (context, index) {
-        return Container(
-          color: colors[index],
-          child: Center(
-              child: Text(
-                "Screen $index",
-                style: const TextStyle(fontSize: 68l),
-              )),
-        );
+        return VideoPost(onVideoFinished: _onVideoFinished);
       },
     );
   }
